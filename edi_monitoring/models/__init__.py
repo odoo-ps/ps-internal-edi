a4a8c8216907a3c7313e35e72bfe1c9338b23e2e
mgrenson <mgr@odoo.com>
2022-06-28 09:52:29 +0200

    [ADD] edi_monitoring : Monitoring integrations by emails

---
+++ edi_monitoring/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from . import edi_integration
+from . import edi_monitoring
+from . import edi_synchronization
+
---
+++ edi_monitoring/models/edi_synchronization.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class Synchronization(models.Model):
+    _inherit = 'edi.synchronization'
+
+    file_size = fields.Integer(help='File size (bytes)')
+    monitoring_report_id = fields.Many2one('edi.monitoring.report')
+    last_issue = fields.Text(compute='_compute_last_issue')
+
+    @api.depends('error_ids.description')
+    def _compute_last_issue(self):
+        for rec in self:
+            rec.last_issue = rec.error_ids[0].description_short if rec.error_ids else False
