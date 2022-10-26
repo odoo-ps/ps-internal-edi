13571974c8ddeb43e783c4e66e78e0f22c964265
Simon Calbert <sct@odoo.com>
2022-10-26 10:53:22 +0200

    [IMP,FIX] edi_base,edi_ftp_connection: fix ftp_in_done_let + imp perfs
    When ftp_in_done_let is True, files are let in the in folder
    To avoid re-processing them in a future synchronization,
    we fitler them to remove those linked to an existing
    synchronization (in state done).
    However, if the synchronizations are archived, the filter will not work
    and the file will be re-processed.
    Now, all synchronizations (even the archived ones) are taken into account,
    and performance of the filter has been improved by using one unique SQL query

---
+++ edi_ftp_connection/migrations/15.0.1.1.0/pre-migration.py
@@ -0,0 +1,18 @@
+import logging
+
+from odoo import SUPERUSER_ID, api
+
+logger = logging.getLogger(__name__)
+
+
+def migrate(cr, version):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+
+    # reset synchronization_creation to 1 for all IN FTP integrations with ftp_in_done_let = True
+    logger.info("Reset synchronization_creation to 1 for all IN FTP integrations with ftp_in_done_let = True")
+    env.with_context(active_test=False).search(
+        [
+            ("integration_flow", "in", env["edi.integration"]._get_in_flow_type()),
+            ("connection_id.ftp_in_done_let", "=", True),
+        ]
+    ).write({"synchronization_creation": 1})
