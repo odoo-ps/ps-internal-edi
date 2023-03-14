f8770673fdc4a4cf39384da2b772a3fe55f7acd2
Simon Calbert <sct@odoo.com>
2023-03-14 11:01:22 +0100

    [IMP] edi_archiving: use action_archive

--- edi_archiving/models/edi_synchronization.py
+++ edi_archiving/models/edi_synchronization.py
@@ -30,7 +30,7 @@ class Synchronization(models.Model):
             return True
         domain += self._archive_states_domain(states)
 
-        self.search(domain).active = False
+        self.search(domain).action_archive()
         return True
 
     @api.model
