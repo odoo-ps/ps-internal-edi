f043d129291852fedffcff417aa56c0f849adc69
mgrenson <mgr@odoo.com>
2023-07-04 08:25:56 +0200

    [REF] edi_base : Move raise errors in a separated method

--- edi_base/models/edi_integration.py
+++ edi_base/models/edi_integration.py
@@ -498,17 +498,21 @@ class Integration(models.Model):
             self.env.activity = "Set Status"
             self._set_status()
 
-            self._safe_commit()
-            if autocommit:
-                new_cr.close()
-
             # logging + traceback
             if not self.env.context.get("no_exception_log"):
                 for e in exceptions:
                     _logger.exception(ustr(e))
+            self._process_in_out_raise_errors(exceptions, raise_error)
 
-            if exceptions and raise_error:
-                raise UserError("\n".join(map(str, exceptions)))
+            self._safe_commit()
+            if autocommit:
+                new_cr.close()
+
+    def _process_in_out_raise_errors(self, exceptions, raise_error):
+        """Separated method to allow to define specific behaviour in case of errors (send emails...)"""
+        self.ensure_one()
+        if exceptions and raise_error:
+            raise UserError("\n".join(map(str, exceptions)))
 
     def _process_data(self, data=None, raise_error=False):
         """Get and Process data (in/out)
