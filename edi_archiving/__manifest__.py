8cd6cf8a599498a60eb51e63b648414a59e4efa6
Stanislas Gueniffey <stgu@odoo.com>
2024-02-27 13:34:18 +0100

    [IMP] edi_archiving: adapt settings style
    NOTE that some <div> ids have changed
    - edi_archiving_duration moved inside the setting
    - edi_archive_state_fail removed because it no longer makes sense
    
    see odoo/odoo#106425

--- edi_archiving/__manifest__.py
+++ edi_archiving/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Odoo PS EDI Archiving",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "license": "OEEL-1",
     "summary": "Automatically archive synchronizations",
     "category": "Tools",
