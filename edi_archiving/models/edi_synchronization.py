2b3e71f8efaac7022acfada368a72d921b455b73
mgrenson <mgr@odoo.com>
2024-10-10 16:33:43 +0200

    [IMP] edi_archiving : Archive without prefetch all data, limit it and delete the content
    Before this commit we faced 3 issues :
    - standard `action_archive()` generated a memory issue with a few hundred records, because all colums were loaded just to check the active one
    - no reasonable limit was defined to avoid the process from being too long
    - the database kept growing and growing for integrations using "Write content on synchronization", which was historically the default behaviour
    
    Now we have 3 improvements :
    - avoid from prefetching, so the memory of the server says thank you
    - a limit of 100k records was defined, and the rest is now delegated to the next execution which is triggered if necessary
    - the content of the synchronization (the data sent or received) is now deleted during the archiving process
    
    **Note1 :**
    We still have huge SQL select by batch of 1000, but it's the ORM limit.
    In this case it's not worth it to go on SQL directly :
    - it's fast now
    - we have depends to archive errors
    
    **Note2 :**
    We had debates in the past to avoid from writing the content of synchronizations by default.
    FInal decision was not changing it.
    So this solution seems to be the most reasonable when we want to get rid of some data wich became useless.
    I hesitated to inherit the `action_archive()` of `edi.synchronization` to delete the content (to cover for ex. the archive action from the interface), but I decided not to change some default behavior, and only do it explicitely with edi_archving module.

--- edi_archiving/models/edi_synchronization.py
+++ edi_archiving/models/edi_synchronization.py
@@ -15,7 +15,11 @@ class Synchronization(models.Model):
 
     @api.model
     def archive_outdated_synchronizations(self):
-        """Call by cron task to archive synchronizations having reach the duration"""
+        """Call by cron task to archive synchronizations having reach the duration
+        - Limit the execution but force the cron to flush the rest the minute after
+        - Empty the content sent or received at the same time
+        """
+        limit = 100000
         config = self.env["ir.config_parameter"].sudo()
 
         # consider the configured duration
@@ -32,7 +36,16 @@ class Synchronization(models.Model):
             return True
         domain += self._archive_states_domain(states)
 
-        self.search(domain).action_archive()
+        records = self.search(domain, limit=limit+1)
+        records[:limit].with_context(prefetch_fields=False).action_archive()  # boom without prefetch with few hundreds
+
+        # free some space if the content sent or received was stored
+        records[:limit].content = False
+
+        if len(records) > limit:
+            cron = self.env.ref("edi_archiving.archive_outdated_synchronizations_cron")
+            if not self.env["ir.cron.trigger"].sudo().search_count([("cron_id", "in", cron.ids)]):
+                cron._trigger()
         return True
 
     @api.model
