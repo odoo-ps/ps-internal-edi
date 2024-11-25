47554e539547b6980225e14e07099b229af5ba03
mgrenson <mgr@odoo.com>
2024-11-25 16:09:54 +0100

    [IMP] edi_archiving : Allow defining a delete policy
    Before, old synchronizations were kept forever. Even without the content, it will grow and grow, and most of the time old synchronizations are totally useless for the user.
    
    Now we give the user the possibility to define a max duration for synchronizations to be deleted.
    
    closes #37

--- edi_archiving/__manifest__.py
+++ edi_archiving/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Odoo PS EDI Archiving",
-    "version": "17.0.1.1.0",
+    "version": "17.0.1.2.0",
     "license": "OEEL-1",
     "summary": "Automatically archive synchronizations",
     "category": "Tools",
