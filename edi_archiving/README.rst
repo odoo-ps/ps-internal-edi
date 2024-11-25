2a0e6e082394c224406a2b2e5b6c317f1b97535e
mgrenson <mgr@odoo.com>
2024-11-25 15:29:33 +0100

    [REF] edi_archiving : Rely on standard vacuum
    Before we were relying on a specific cron task to handle the archiving.
    This cron task had to be active, so it was activated or archived on the fly depending on the archiving settings.
    
    Now it's simplified, by relying on standard behavior.
    Since this vacuum is shared with other methods, the amount of synchronizations to archive in one batch was reduced.
    But since it will be archived at next execution thanks to cron trigger, it's not important.

--- edi_archiving/README.rst
+++ edi_archiving/README.rst
@@ -1,11 +1,11 @@
-Automatically archive synchronizations :
+## Automatically archive synchronizations
 
 - add active field on synchronizations & synchronizations_errors
 - allow to choose duration before archive synchronizations, by status
-- let a cron task archive them depending on the configuration
+- let internal odoo vacuum archive them depending on the configuration
 - empty the content sent or received at the same time
   (to limit the DB from growing infinitely, considering that it could have been useful for debug,
   but once we decide to archive we agree to loose that information)
 
-When archiving, only 100.000 records are considered at the same time.
+When archiving, only 10.000 records are considered at the same time.
 If there is more than than (initial usage for ex), the execution is delegated to the next execution the minute after.
--- edi_archiving/models/edi_synchronization.py
+++ edi_archiving/models/edi_synchronization.py
@@ -13,39 +13,36 @@ class Synchronization(models.Model):
     active = fields.Boolean(default=True)
     error_ids = fields.One2many(context={"active_test": False})  # consider archived errors too
 
-    @api.model
-    def archive_outdated_synchronizations(self):
+    @api.autovacuum
+    def _archive_outdated_synchronizations(self):
         """Call by cron task to archive synchronizations having reach the duration
         - Limit the execution but force the cron to flush the rest the minute after
         - Empty the content sent or received at the same time
         """
-        limit = 100000
+        limit = 10000
         config = self.env["ir.config_parameter"].sudo()
 
         # consider the configured duration
         duration = int(config.get_param("edi.archive.duration", 0))
         if not duration:
-            _logger.warning("Cron task archive_outdated_synchronizations_cron should be archived : duration=0")
             return True
         domain = [("create_date", "<=", fields.Datetime.now() - timedelta(days=duration))]
 
         # only consider the configured states
         states = [x for x in self._archive_states() if config.get_param("edi.archive.state.%s" % x, False)]
         if not states:
-            _logger.warning("Cron task archive_outdated_synchronizations_cron should be archived : no state configured")
+            # just a security, but should never happen thanks to check on settings side
             return True
         domain += self._archive_states_domain(states)
 
-        records = self.search(domain, limit=limit+1)
+        records = self.search(domain, limit=limit + 1)
         records[:limit].with_context(prefetch_fields=False).action_archive()  # boom without prefetch with few hundreds
 
         # free some space if the content sent or received was stored
         records[:limit].content = False
 
         if len(records) > limit:
-            cron = self.env.ref("edi_archiving.archive_outdated_synchronizations_cron")
-            if not self.env["ir.cron.trigger"].sudo().search_count([("cron_id", "in", cron.ids)]):
-                cron._trigger()
+            self.env.ref("base.autovacuum_job")._trigger()
         return True
 
     @api.model
