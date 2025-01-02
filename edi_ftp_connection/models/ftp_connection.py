a94efa312793a96dfd6b0e6616bf22243b46d3d1
Romain Derie <rde@odoo.com>
2025-01-02 14:13:16 +0100

    [IMP] edi_ftp_connection: add file open encoding option in config
    For a client, it's needed to force the encoding to `ISO-8859-1`.
    Without this, the following error is shown:
    ```
    'utf-8' codec can't decode byte 0xe9 in position 566: invalid continuation byte
    ```
    
    The workaround in the meantime was to fully override the method
    `_ftp_fetch_files` to "just" force the encoding, which is obviously not
    ideal.
    
    This will allow anyone to force the encoding by simply adding it to the
    `edi.connection` config.
    
    Note that the choice was made to set the encoding on the server directly
    and to reuse it later when we need it. It means that filenames and
    directories will also be read with that encoding by `ftplib`.
    Should that be a problem at some point (the need to really have distinct
    encoding for the file/folder names and file content), we will separate
    those.

--- edi_ftp_connection/models/ftp_connection.py
+++ edi_ftp_connection/models/ftp_connection.py
@@ -102,6 +102,10 @@ class FTPConnection(models.Model):
 
         config = self._read_configuration()
         server = ftplib.FTP(host=config["host"], user=config["user"], passwd=config["password"])
+
+        if config.get("encoding"):
+            server.encoding = config["encoding"]
+
         if config.get("port"):
             server.port = int(config["port"])
 
@@ -302,7 +306,7 @@ class FTPConnection(models.Model):
                     raise UserError(_("Fetch synchronization failed for file %s:\n%s") % (filename, ustr(e)))
         if self.ftp_load_content:
             for res in result:
-                with open(res["file"], "r") as f:
+                with open(res["file"], "r", encoding=server.encoding) as f:
                     res["content"] = f.read()
         return result
 
