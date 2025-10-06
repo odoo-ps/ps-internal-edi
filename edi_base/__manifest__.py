cf8278b49abdfd192ac8ea5183167f0288fafd76
Michel Grenson (mgr) <mgr@odoo.com>
2025-10-06 16:17:50 +0200

    [REF] edi_base,test_edi_base : Better handling of integration state & nicer kanban
    Mainly, simplify the way of setting the status on the integration after one synchronization is executed :
    - Avoid ancestral SQL queries
    - instead, just rely on existing synchronization which was created just before
    
    Additionally, it did require to refactor few parts to make it consistent :
    - Add some real structured state on integration (last_state), based on state from last synchronization
    - Remove former last_sync_status which was just a char
    - Remove color fields & replace that by more appropriate classes depending on the state
    - Review kanban view of integrations to make it more beautiful
    - Remove kanban view of synchronizations (why would we list thousands of synchronizations with a kanban anyway...)
    
    And just for fun :
    - Avoid from copying the value of readonly fields when duplicating

--- edi_base/__manifest__.py
+++ edi_base/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Odoo PS EDI Framework",
-    "version": "19.0.1.2.0",
+    "version": "19.0.1.3.0",
     "license": "OEEL-1",
     "summary": "Gateway between odoo and third party components",
     "category": "Tools",
