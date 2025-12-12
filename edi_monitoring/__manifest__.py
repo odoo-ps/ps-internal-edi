b847f75f1f15e0822cb3a43769eae99c1bb1fbf8
Killian Frappart <killianfrappart@Killians-MacBook-Pro.local>
2025-12-12 10:50:21 +0100

    [FIX] edi_monitoring: Subject may not contain linefeed or carriage return characters

--- edi_monitoring/__manifest__.py
+++ edi_monitoring/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Odoo PS EDI Monitoring",
-    "version": "19.0.1.0.1",
+    "version": "19.0.1.0.2",
     "license": "OEEL-1",
     "summary": "Monitoring integrations & sending reports by email",
     "category": "Tools",
