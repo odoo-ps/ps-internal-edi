1d1f3735986a5e27c8530ff5ac8afb3bc1d0ee95
Michel Grenson (mgr) <mgr@odoo.com>
2026-05-26 15:39:20 +0200

    [UPG-19.3] edi_base : Adapt script version
    Before :
    WARNING odoo20_edi_basis odoo.modules.migration: Invalid version for upgrade script '/home/odoo/git_repo/odoo-ps/ps-internal-edi/edi_base/migrations/19.3.1.6.0
    
    Reason :
    VERSION_RE only accepts prefixes of main versions (19.0, 18.0...).
    19.3 does not match, so the script would never be executed.
    cf. https://github.com/odoo/odoo/blob/19.0/odoo/modules/migration.py#L31
    
    Now :
    The script is executed, and the fact that the version of the module is 19.3.1.6.0 (vs script 19.0.1.6.0) does not cause any issue.

--- edi_base/migrations/19.0.1.6.0/pre-migration.py
+++ edi_base/migrations/19.0.1.6.0/pre-migration.py
@@ -1,7 +1,5 @@
 def migrate(cr, version):
-    cr.execute(
-        """
+    cr.execute("""
         UPDATE edi_integration
         SET synchronization_content_type = 'txt' WHERE synchronization_content_type = 'text'
-    """
-    )
+    """)
