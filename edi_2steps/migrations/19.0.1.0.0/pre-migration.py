1c9494542731774213fa984c46f1cbc7139d7e4d
Augusto Perez <aup@odoo.com>
2026-06-23 13:34:49 +0200

    [UPG][5243578] edi_2steps: Upgrade script for renamed views

---
+++ edi_2steps/migrations/19.0.1.0.0/pre-migration.py
@@ -0,0 +1,16 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    views_to_rename = [
+        ("integration_form_view_edi_table", "integration_form_view"),
+        ("synchronization_form_view_edi_table", "synchronization_form_view"),
+    ]
+    for old_xmlid, new_xmlid in views_to_rename:
+        util.rename_xmlid(cr, f"edi_2steps.{old_xmlid}", f"edi_2steps.{new_xmlid}")
+
+    views_to_remove = [
+        "res_config_settings_view_form_edi_table",
+    ]
+    for view in views_to_remove:
+        util.remove_view(cr, f"edi_2steps.{view}")
